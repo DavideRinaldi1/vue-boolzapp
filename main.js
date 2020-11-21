b3d870b050b56e82e8b4cf7678bb5a5f61679e1a
Davide <rinaldi.davide94@gmail.com>
2020-11-21 17:55:14 +0100

    add array contact

--- main.js
+++ main.js
@@ -7,7 +7,38 @@ const app = new Vue({
         },
     
         contactArr: [
-            
+            {
+                profileImg: 'img/avatar_1.jpg',
+                name: 'Giovanni'
+            },
+            {
+                profileImg: 'img/avatar_2.jpg',
+                name: 'Michele'
+            },
+            {
+                profileImg: 'img/avatar_3.jpg',
+                name: 'Francesco'
+            },
+            {
+                profileImg: 'img/avatar_4.jpg',
+                name: 'Donatella'
+            },
+            {
+                profileImg: 'img/avatar_5.jpg',
+                name: 'Franca'
+            },
+            {
+                profileImg: 'img/avatar_6.jpg',
+                name: 'Jhonny'
+            },
+            {
+                profileImg: 'img/avatar_8.jpg',
+                name: 'Federico'
+            },
+            {
+                profileImg: 'img/avatar_io.jpg',
+                name: 'Giorgia'
+            }
         ]
     }      
 })
