a71f0601880c3ca779aa80e22cb02d733d6ec587
Davide <rinaldi.davide94@gmail.com>
2020-11-21 12:17:49 +0100

    fixed html, init js

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#root',
+    data:{
+        user: {
+            profileImg: 'img/avatar_7.jpg',
+            name: 'Davide Rinaldi'
+        },
+    
+        contactArr: [
+            
+        ]
+    }      
+})
